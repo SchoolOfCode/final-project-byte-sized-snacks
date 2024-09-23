fde6f3d180811dbd4cbfea0c56006737c9c38111
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-09-23 18:00:22 +0100

    feat: set up Supabase database and connect to project

---
+++ smart-fridge-ai/backend/connection.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL
+});
+
+client.connect()
+  .then(() => console.log('Connected to the database!'))
+  .catch(err => console.error('Connection error', err.stack))
+  .finally(() => client.end());
