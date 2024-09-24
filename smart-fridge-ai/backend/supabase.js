57e0e7f4ef29d2053bee6201d2f493836f4d05d9
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-09-24 09:45:34 +0100

    Remove sensitive information from supabase.js

--- smart-fridge-ai/backend/supabase.js
+++ smart-fridge-ai/backend/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yjgbkibidmshqjqibnjr.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqZ2JraWJpZG1zaHFqcWlibmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMTAxODcsImV4cCI6MjA0MjY4NjE4N30.eeYIZ31ln4R6rMSw7gkj80BCaKIj57qxy0v4Ag1y_Y4'
+const supabaseUrl = 
+const supabaseAnonKey = 
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
