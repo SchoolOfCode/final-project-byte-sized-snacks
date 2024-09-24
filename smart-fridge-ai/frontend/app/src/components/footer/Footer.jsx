fd0da9ecf6cd323c9e81be62c608de814d1dba6b
daffodil009 <daffodil.luck9@gmail.com>
2024-09-24 12:40:14 +0100

    Footer pagelinks

--- smart-fridge-ai/frontend/app/src/components/footer/Footer.jsx
+++ smart-fridge-ai/frontend/app/src/components/footer/Footer.jsx
@@ -1,1 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+import styles from "./Footer.module.css";
+import ExternalLink from './ExternalLink/ExternalLink';
+import Link from './Link/Link';
+
+
+const Footer = () => {
+
+    const pageLinks = [
+        { href: "http://localhost/3000/home", text: "🏚️Home" },
+        { href : "http://localhost/3000/item", text: "📃Add item" },
+        { href: "http://localhost/3000/inventory", text: "🍇🍓🍌Inventory" },
+        { href: "http://localhost/3000/shopping", text: "🛒Shopping" },
+        
+    ];
+
+}
+
+
+return (
+    <footer className={styles.footer}>
+
+        
+        <ul>
+            {pageLinks.map((link, index) => (
+                <Link key={index} href={link.href} text={link.text}>
+
+                </Link>
+            ))}
+        </ul>
+
+    </footer>
+        
+)
